ab4ffd6df2a7e32be855e85f24ebbb6637fc74a7
David <54381677+DLis6@users.noreply.github.com>
2019-09-30 19:24:53 -0500

    Heroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 
 app.get('/', (req, res) => {
     res.send(req.header('User-Agent'));
 });
 
-app.listen(3000, () => console.log("Listening 3000"));
+app.listen(PORT, () => console.log('Listening on port 3000'))
+    //app.listen(3000, () => console.log("Listening 3000"));
